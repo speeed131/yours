7cba6a1ccffdfc9879c2348ef64f0b63cd09fbe7
Daiki Hirose <speeed131@gmail.com>
2021-10-10 14:47:36 +0900

    dailyのAPIルーター定義

--- api/routers/daily.py
+++ api/routers/daily.py
@@ -1,46 +1,55 @@
 from typing import Any, List
 from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-import api.schemas.daily as schemas
+from api.db import get_db
+
+import api.schemas.daily as schemas_daily
 import api.schemas.auth as schemas_auth
 import api.cruds.auth as cruds_auth
+import api.cruds.daily as cruds_daily
 
 
 router = APIRouter()
 
 
-@router.get("/dailies", response_model=List[schemas.Daily])
-def get_dailies(
-    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user)
+@router.get("/dailies", response_model=List[schemas_daily.Daily])
+async def get_dailies(
+    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user),
+    db: AsyncSession = Depends(get_db)
 ):
     # [バックエンド] メモ一覧表示のAPI
-    return List[schemas.Daily]
+    return await cruds_daily.get_dailies_list_by_user(current_user, db)
 
 
-@router.get("/dailies/{daily_id}", response_model=List[schemas.Daily])
-def get_daily_detail(
+@router.get("/dailies/{daily_id}", response_model=List[schemas_daily.Daily])
+async def get_daily_detail(
     daily_id: int,
-    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user)
+    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user),
+    db: AsyncSession = Depends(get_db)
 ):
     # [バックエンド] メモ詳細表示のAPI
-    # １,画像をapiを使って取得する
-    # ２，取得した画像を返す処理
-    return schemas.Daily
+    return await cruds_daily.get_daily_by_daily_id(daily_id, current_user, db)
 
 
-@router.get("/dailies/create", response_model=schemas.Daily)
-def create_daily(
-    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user)
+@router.get("/dailies/create", response_model=schemas_daily.Daily)
+async def create_daily(
+    daily_data: schemas_daily.Daily,
+    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user),
+    db: AsyncSession = Depends(get_db)
+
 ):
     # [バックエンド] メモの新規作成機能
+    return await cruds_daily.create_daily(daily_data, current_user, db)
 
-    return schemas.Daily
 
+@router.get("/dailies/{daily_id}/delete", response_model=schemas_daily.Daily)
+async def delete_daily(
+    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user),
+    db: AsyncSession = Depends(get_db)
 
-@router.get("/dailies/{daily_id}/delete", response_model=schemas.Daily)
-def delete_daily(
-    current_user: schemas_auth.User = Depends(cruds_auth.get_current_user)
 ):
     # [バックエンド] メモの削除
-
-    return schemas.Daily
+    daily = await cruds_daily.get_daily_by_daily_id(daily_id, current_user, db)
+    result = await cruds_daily.delete_daily_by_daily_id(daily, current_user, db)
+    return result

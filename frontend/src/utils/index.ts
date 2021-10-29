6bbfc9f5960de20af6fea6778584a35141bb72f3
Daiki Hirose <speeed131@gmail.com>
2021-10-29 20:24:55 +0900

    環境をvuecliで作り直し

--- frontend/src/utils/index.ts
+++
@@ -1,6 +0,0 @@
-export const utils = {
-    getLocalToken: () => localStorage.getItem("token"),
-    saveLocalToken: (token: string) => localStorage.setItem("token", token),
-    removeLocalToken: () => localStorage.removeItem("token"),
-  };
-  

3587e596edfaedaba8a97ebe720261d351a6891b
Daiki Hirose <speeed131@gmail.com>
2021-10-29 20:37:38 +0900

    元の作ったフォルダを戻す

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,10 @@
+import { auth } from "./auth"
+import { daily } from "./daily"
+import { word } from "./word"
+
+
+export const api = {
+    auth,
+    daily,
+    word
+}
---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,6 @@
+export const utils = {
+    getLocalToken: () => localStorage.getItem("token"),
+    saveLocalToken: (token: string) => localStorage.setItem("token", token),
+    removeLocalToken: () => localStorage.removeItem("token"),
+  };
+  

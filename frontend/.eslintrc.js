c14c9a5caa88b85463440f229fd672c6e93965f1
Daiki Hirose <speeed131@gmail.com>
2021-11-12 18:36:25 +0900

    eslint設定変更

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     node: true,
   },
   extends: [
+    // https://future-architect.github.io/articles/20210616a/
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
+    // "@vue/prettier", //@MEMO: テンプレート内で、クラス多いと改行してくるためコメントアウト
     "@vue/prettier/@typescript-eslint",
+    "prettier"
   ],
   parserOptions: {
     ecmaVersion: 2020,
@@ -16,6 +18,10 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // @see https://eslint.vuejs.org/rules/singleline-html-element-content-newline.html
+    "vue/singleline-html-element-content-newline": "off",
+    // @see https://eslint.vuejs.org/rules/multiline-html-element-content-newline.html#vue-multiline-html-element-content-newline
+    "vue/multiline-html-element-content-newline": "off"
   },
   overrides: [
     {

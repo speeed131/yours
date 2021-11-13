0e9ddee5b74ba9ad2fe6d849c1accfa195c2da30
Daiki Hirose <speeed131@gmail.com>
2021-11-13 18:42:21 +0900

    login前のページを仮で作成

---
+++ frontend/src/pages/beforelogin/register.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="About">
+    {{ msg }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+
+export default defineComponent({
+  name: "About",
+  components: {},
+  setup() {
+    const state = reactive({
+      msg: "Hello TypeScript",
+    });
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>

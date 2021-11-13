ed62bf1394242c67d8cbaee791409cd1436b839c
Daiki Hirose <speeed131@gmail.com>
2021-11-13 12:02:03 +0900

    仮でstore呼び出し

--- frontend/src/pages/word/WordList.vue
+++ frontend/src/pages/word/WordList.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="WordList">
+  <div class="WordList" @click="dispatchGetWords()">
     {{ msg }}
   </div>
 </template>
 
 <script lang="ts">
+import { IWord } from "@/interfaces/api";
 import { defineComponent, reactive, toRefs } from "vue";
+import { useStore } from "vuex"
 
 export default defineComponent({
   name: "WordList",
   components: {},
   setup() {
+    const store = useStore()
     const state = reactive({
       msg: "Hello TypeScript",
     });
     return {
       ...toRefs(state),
+
+      //一応呼び出しはできた。 ログインと登録機能先に作る必要ありそう
+      dispatchGetWords: () => {
+        store.dispatch("word/getWords")
+      }
+
     };
   },
+  // computed: {
+
+  // }
 });
 </script>

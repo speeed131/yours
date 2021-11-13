81106b2f6753808f8d7b7cb69d0aadcce4c26c7f
Daiki Hirose <speeed131@gmail.com>
2021-11-14 04:59:33 +0900

    auth系のルーター作成

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,15 +6,40 @@ import DefaultLayout from "@/layouts/DefaultLayout.vue";
 import WordList from "@/pages/word/WordList.vue";
 import WordListToRemember from "@/pages/word/WordListToRemember.vue";
 import DailyList from "@/pages/daily/DailyList.vue";
+import Login from "@/pages/beforelogin/login.vue"
+import Register from "@/pages/beforelogin/register.vue"
+import Landing from "@/pages/beforelogin/landing.vue"
+import { api } from "@/api/index"
+
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Layout",
-    redirect: "/home",
+    redirect: "/login",
+    beforeEnter: (to, from) => {
+      // reject the navigation
+      console.log(api.auth.getUserMe())
+      if (!api.auth.getUserMe()) return "/login";
+    },
     component: DefaultLayout,
     children: [
-      //https://next.router.vuejs.org/guide/essentials/nested-routes.html
+      //https://next.router.vuejs.org/guide/essentials/nested-routes.htm
+      {
+        path: "login",
+        name: "Login",
+        component: Login,
+      },
+      {
+        path: "register",
+        name: "Register",
+        component: Register,
+      },
+      {
+        path: "landing",
+        name: "Landing",
+        component: Landing,
+      },
       {
         path: "home",
         name: "Home",

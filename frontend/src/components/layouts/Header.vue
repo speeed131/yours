a7d50daabc39a87ee3b2d0ed7736a2592b4504de
Daiki Hirose <speeed131@gmail.com>
2021-11-20 14:42:36 +0900

    header作成

--- frontend/src/components/layouts/Header.vue
+++ frontend/src/components/layouts/Header.vue
@@ -1,98 +1,66 @@
+<!-- Navbar -->
 <template>
-  <!-- Navbar -->
-  <nav
-    class="
-      absolute
-      top-0
-      left-0
-      w-full
-      z-10
-      bg-transparent
-      md:flex-row md:flex-nowrap md:justify-start
-      flex
-      items-center
-      p-4
-    "
-  >
-    <div
-      class="
-        w-full
-        mx-autp
-        items-center
-        flex
-        justify-between
-        md:flex-nowrap
-        flex-wrap
-        md:px-10
-        px-4
-      "
-    >
-      <!-- Brand -->
-      <!-- <a
-        class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
-        href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/dashboard"
-        >yours</a
-      > -->
-      <!-- Form -->
-      <form
-        class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
-      >
-        <div class="relative flex w-full flex-wrap items-stretch">
-          <span
-            class="
-              z-10
-              h-full
-              leading-snug
-              font-normal
-              absolute
-              text-center text-blueGray-300
-              bg-transparent
-              rounded
-              text-base
-              items-center
-              justify-center
-              w-8
-              pl-3
-              py-3
-            "
-            ><i class="fas fa-search"></i
-          ></span>
-          <input
-            type="text"
-            placeholder="Search here..."
-            class="
-              border-0
-              px-3
-              py-3
-              placeholder-blueGray-300
-              text-blueGray-600
-              relative
-              bg-white
-              rounded
-              text-sm
-              shadow
-              outline-none
-              focus:outline-none focus:ring
-              w-full
-              pl-10
-            "
-          />
+  <div class="bg-yellow-500">
+    <div class="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full md:px-24 lg:px-1">
+      <div class="relative flex items-center justify-between">
+        <div class="flex items-center">
+          <a
+            href="/"
+            aria-label="Company"
+            title="Company"
+            class="inline-flex items-center mr-8"
+          >
+            <span class="ml-2 text-xl font-bold tracking-wide text-gray-100"
+              >Yours</span
+            >
+          </a>
         </div>
-      </form>
-      <!-- User -->
-      <ul class="flex-col md:flex-row list-none items-center hidden md:flex">
-        <user-dropdown-component></user-dropdown-component>
-      </ul>
+        <ul v-if="user" class="flex items-center hidden space-x-8 lg:flex">
+          <li>
+            <a
+              href="/"
+              aria-label="Sign up"
+              title="Sign up"
+              class="
+                font-bold
+                tracking-wide
+                text-gray-100
+                transition-colors
+                duration-200
+                hover:text-teal-accent-400
+              "
+            >
+              ユーザー登録
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              aria-label="Sign in"
+              title="Sign in"
+              class="
+                font-bold
+                tracking-wide
+                text-gray-100
+                transition-colors
+                duration-200
+                hover:text-teal-accent-400
+              "
+              >ログイン</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
-  </nav>
-
-  <!-- End Navbar -->
+  </div>
 </template>
 <script>
-import UserDropdownComponent from "@/components/UserDropdown.vue";
 export default {
-  components: {
-    UserDropdownComponent,
+  data() {
+    return {
+      isMenuOpen: false,
+      user: true,
+    };
   },
 };
 </script>

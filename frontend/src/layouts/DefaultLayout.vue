2322b35efc9bbc2aa5aae42be0d014ee0699f0e2
Daiki Hirose <speeed131@gmail.com>
2021-11-20 14:43:52 +0900

    layoutを作成

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,3 +1,34 @@
 <template>
-  <router-view />
+  <Header></Header>
+  <div class="relative bg-white dark:bg-gray-800">
+    <div class="flex justify-between">
+      <div v-if="false" class="flex-grow">
+        <Sideba />
+      </div>
+      <div class="flex-grow">
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+
+import Header from "@/components/layouts/Header.vue";
+import Sidebar from "@/components/layouts/Sidebar.vue";
+export default defineComponent({
+  name: "About",
+  components: { Sidebar, Header },
+  setup() {
+    const state = reactive({
+      msg: "Hello TypeScript",
+    });
+    return {
+      ...toRefs(state),
+    };
+  },
+});
+</script>
+
+<style lang="sass" scoped></style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
-    <Sidebar />
-    <div class="relative md:ml-64 bg-blueGray-100">
-      <Header></Header>
+    <!-- <Sidebar /> -->
+    <!-- <Sidebar :visible="true" class="p-sidebar-lg"> Content </Sidebar> -->
+
+    <!-- <Header></Header> -->
+    <div class="relative bg-blueGray-100">
       <!-- Header -->
       <div class="relative bg-pink-600 md:pt-32 pb-32 pt-12">
         <div class="px-4 md:px-10 mx-auto w-full">
@@ -1138,122 +1140,21 @@
             </div>
           </div>
         </div>
-        <footer class="block py-4">
-          <div class="container mx-auto px-4">
-            <hr class="mb-4 border-b-1 border-blueGray-200" />
-            <div
-              class="
-                flex flex-wrap
-                items-center
-                md:justify-between
-                justify-center
-              "
-            >
-              <div class="w-full md:w-4/12 px-4">
-                <div class="text-sm text-blueGray-500 font-semibold py-1">
-                  Copyright © {{ date }}
-                  <a
-                    href="https://www.creative-tim.com"
-                    class="
-                      text-blueGray-500
-                      hover:text-blueGray-700
-                      text-sm
-                      font-semibold
-                      py-1
-                    "
-                  >
-                    Creative Tim
-                  </a>
-                </div>
-              </div>
-              <div class="w-full md:w-8/12 px-4">
-                <ul
-                  class="flex flex-wrap list-none md:justify-end justify-center"
-                >
-                  <li>
-                    <a
-                      href="https://www.creative-tim.com"
-                      class="
-                        text-blueGray-600
-                        hover:text-blueGray-800
-                        text-sm
-                        font-semibold
-                        block
-                        py-1
-                        px-3
-                      "
-                    >
-                      Creative Tim
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.creative-tim.com/presentation"
-                      class="
-                        text-blueGray-600
-                        hover:text-blueGray-800
-                        text-sm
-                        font-semibold
-                        block
-                        py-1
-                        px-3
-                      "
-                    >
-                      About Us
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="http://blog.creative-tim.com"
-                      class="
-                        text-blueGray-600
-                        hover:text-blueGray-800
-                        text-sm
-                        font-semibold
-                        block
-                        py-1
-                        px-3
-                      "
-                    >
-                      Blog
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://github.com/creativetimofficial/tailwind-starter-kit/blob/main/LICENSE.md"
-                      class="
-                        text-blueGray-600
-                        hover:text-blueGray-800
-                        text-sm
-                        font-semibold
-                        block
-                        py-1
-                        px-3
-                      "
-                    >
-                      MIT License
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </footer>
       </div>
     </div>
   </div>
 </template>
 <script>
-import Header from "../components/layouts/Header.vue";
-import Sidebar from "../components/layouts/Sidebar.vue";
+// import Header from "../components/layouts/Header.vue";
+// import Sidebar from "../components/layouts/Sidebar.vue";
+// import Sidebar from "primevue/sidebar";
+
 import LineChartComponent from "../components/LineChart.vue";
 import BarChartComponent from "../components/BarChart.vue";
 
 export default {
   name: "dashboard-page",
   components: {
-    Header,
-    Sidebar,
     LineChartComponent,
     BarChartComponent,
   },

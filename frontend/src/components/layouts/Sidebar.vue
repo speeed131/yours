9c0006825a17fc7c5f62a6770de7d66bf873adfc
Daiki Hirose <speeed131@gmail.com>
2021-11-20 14:42:51 +0900

    sidebar作成

--- frontend/src/components/layouts/Sidebar.vue
+++ frontend/src/components/layouts/Sidebar.vue
@@ -1,224 +1,40 @@
 <template>
-  <nav
-    class="
-      md:left-0
-      md:block
-      md:fixed
-      md:top-0
-      md:bottom-0
-      md:overflow-y-auto
-      md:flex-row
-      md:flex-nowrap
-      md:overflow-hidden
-      shadow-xl
-      bg-white
-      flex flex-wrap
-      items-center
-      justify-between
-      relative
-      md:w-64
-      z-10
-      py-4
-      px-6
-    "
-  >
-    <div
-      class="
-        md:flex-col md:items-stretch md:min-h-full md:flex-nowrap
-        px-0
-        flex flex-wrap
-        items-center
-        justify-between
-        w-full
-        mx-auto
-      "
-    >
-      <!-- Toggler -->
-      <button
-        class="
-          cursor-pointer
-          text-black
-          opacity-50
-          md:hidden
-          px-3
-          py-1
-          text-xl
-          leading-none
-          bg-transparent
-          rounded
-          border border-solid border-transparent
-        "
-        type="button"
-        v-on:click="toggleCollapseShow('bg-white m-2 py-3 px-6')"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
-      <!-- Brand -->
-      <a
-        class="
-          md:block
-          text-left
-          md:pb-2
-          text-blueGray-600
-          mr-0
-          inline-block
-          whitespace-nowrap
-          text-sm
-          uppercase
-          font-bold
-          p-4
-          px-0
-        "
-        href="javascript:void(0)"
-      >
-        yours
-      </a>
-      <!-- User -->
-      <ul class="md:hidden items-center flex flex-wrap list-none">
-        <li class="inline-block relative">
-          <notification-dropdown-component></notification-dropdown-component>
-        </li>
-        <li class="inline-block relative">
-          <user-dropdown-component></user-dropdown-component>
-        </li>
-      </ul>
-      <!-- Collapse -->
-      <div
-        class="
-          md:flex
-          md:flex-col
-          md:items-stretch
-          md:opacity-100
-          md:relative
-          md:mt-4
-          md:shadow-none
-          shadow
-          absolute
-          top-0
-          left-0
-          right-0
-          z-40
-          overflow-y-auto overflow-x-hidden
-          h-auto
-          items-center
-          flex-1
-          rounded
-        "
-        v-bind:class="collapseShow"
-      >
-        <!-- Collapse header -->
-        <div
+  <div class="w-64 h-screen">
+    <nav class="mt-10 px-6">
+      <template v-for="(item, i) in sideBarMenu" :key="i">
+        <a
           class="
-            md:min-w-full md:hidden
-            block
-            pb-4
-            mb-4
-            border-b border-solid border-blueGray-200
+            hover:text-gray-800 hover:bg-gray-100
+            flex
+            items-center
+            p-2
+            my-6
+            transition-colors
+            dark:hover:text-white dark:hover:bg-gray-600
+            duration-200
+            text-gray-600
+            dark:text-gray-400
+            rounded-lg
           "
+          @click="toMovePage(item.routeName)"
         >
-          <div class="flex flex-wrap">
-            <div class="w-6/12">
-              <a
-                class="
-                  md:block
-                  text-left
-                  md:pb-2
-                  text-blueGray-600
-                  mr-0
-                  inline-block
-                  whitespace-nowrap
-                  text-sm
-                  uppercase
-                  font-bold
-                  p-4
-                  px-0
-                "
-                href="javascript:void(0)"
-              >
-                yours
-              </a>
-            </div>
-            <div class="w-6/12 flex justify-end">
-              <button
-                type="button"
-                class="
-                  cursor-pointer
-                  text-black
-                  opacity-50
-                  md:hidden
-                  px-3
-                  py-1
-                  text-xl
-                  leading-none
-                  bg-transparent
-                  rounded
-                  border border-solid border-transparent
-                "
-                v-on:click="toggleCollapseShow('hidden')"
-              >
-                <i class="fas fa-times"></i>
-              </button>
-            </div>
-          </div>
-        </div>
-        <!-- Form -->
-        <form class="mt-6 mb-4 md:hidden">
-          <div class="mb-3 pt-0">
-            <input
-              type="text"
-              placeholder="Search"
-              class="
-                border-0
-                px-3
-                py-2
-                h-12
-                border border-solid border-blueGray-500
-                placeholder-blueGray-300
-                text-blueGray-600
-                bg-white
-                rounded
-                text-base
-                leading-snug
-                shadow-none
-                outline-none
-                focus:outline-none
-                w-full
-                font-normal
-              "
-            />
-          </div>
-        </form>
-        <!-- Navigation -->
-        <!-- SideBarNavGroup -->
-        <SideBarNavGroup :side-bar-menu="sideBarMenu" />
-        <!-- Divider -->
-        <hr class="my-4 md:min-w-full" />
-        <!-- Heading -->
-        <!-- <h6 class="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">Documentation</h6> -->
-        <!-- Navigation -->
-        <!-- <ul class="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-          <li class="inline-flex">
-            <a class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold" href="#/documentation/styles"
-              ><i class="fas fa-paint-brush mr-2 text-blueGray-400 text-base"></i>ToDoリスト</a
-            >
-          </li>
-          <li class="inline-flex">
-            <a class="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold" href="#/documentation/alerts"
-              ><i class="fab fa-css3-alt mr-2 text-blueGray-400 text-base"></i>覚えたほうが良い単語一覧</a
-            >
-          </li>
-        </ul> -->
-      </div>
-    </div>
-  </nav>
+          <i class="text-blueGray-400 mr-2 text-sm" :class="item.class"></i>
+          <span class="mx-4 text-md font-normal">
+            {{ item.title }}
+          </span>
+          <span class="flex-grow text-right"> </span>
+        </a>
+      </template>
+    </nav>
+  </div>
 </template>
+
 <script>
-import NotificationDropdownComponent from "@/components/NotificationDropdown.vue";
-import UserDropdownComponent from "@/components/UserDropdown.vue";
-import SideBarNavGroup from "@/components/layouts/SideBarNavGroup.vue";
-export default {
-  data() {
-    return {
+import { defineComponent, reactive, toRefs } from "vue";
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
       collapseShow: "hidden",
       //@TODO: SideBarNavMenuのコンポーネント分割、以下のオブジェクト使用
       sideBarMenu: [
@@ -243,17 +59,15 @@ export default {
           class: "fas fa-fingerprint",
         },
       ],
+    });
+    return {
+      ...toRefs(state),
     };
   },
   methods: {
-    toggleCollapseShow: function (classes) {
-      this.collapseShow = classes;
+    toMovePage(routeName) {
+      this.$router.push({ name: routeName });
     },
   },
-  components: {
-    NotificationDropdownComponent,
-    UserDropdownComponent,
-    SideBarNavGroup,
-  },
-};
+});
 </script>

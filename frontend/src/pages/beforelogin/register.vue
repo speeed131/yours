bf0fac1486b961d043978276101b178938f0f797
Daiki Hirose <speeed131@gmail.com>
2021-11-20 14:43:29 +0900

    新規登録画面作成

--- frontend/src/pages/beforelogin/register.vue
+++ frontend/src/pages/beforelogin/register.vue
@@ -1,22 +1,182 @@
 <template>
-  <div class="About">
-    {{ msg }}
+  <div
+    class="
+      mt-24
+      mx-auto
+      flex flex-col
+      w-full
+      max-w-lg
+      px-4
+      py-8
+      bg-white
+      rounded-lg
+      shadow
+      dark:bg-gray-800
+      sm:px-6
+      md:px-8
+      lg:px-10
+    "
+  >
+    <div
+      class="
+        self-center
+        mb-6
+        text-xl
+        font-light
+        text-gray-600
+        sm:text-2xl
+        dark:text-white
+      "
+    >
+      新規登録
+    </div>
+    <div class="mt-8">
+      <form action="#" autoComplete="off">
+        <div class="flex flex-col mb-2">
+          <div class="flex relative">
+            <span
+              class="
+                rounded-l-md
+                inline-flex
+                items-center
+                px-3
+                border-t
+                bg-white
+                border-l border-b border-gray-300
+                text-gray-500
+                shadow-sm
+                text-sm
+              "
+            >
+              <i class="pi pi-user"></i>
+            </span>
+            <input
+              type="text"
+              id="sign-in-email"
+              class="
+                rounded-r-lg
+                flex-1
+                appearance-none
+                border border-gray-300
+                w-full
+                py-2
+                px-4
+                bg-white
+                text-gray-700
+                placeholder-gray-400
+                shadow-sm
+                text-base
+                focus:outline-none
+                focus:ring-2
+                focus:ring-yellow-600
+                focus:border-transparent
+              "
+              placeholder="ユーザーネーム"
+            />
+          </div>
+        </div>
+        <div class="flex flex-col mb-6">
+          <div class="flex relative">
+            <span
+              class="
+                rounded-l-md
+                inline-flex
+                items-center
+                px-3
+                border-t
+                bg-white
+                border-l border-b border-gray-300
+                text-gray-500
+                shadow-sm
+                text-sm
+              "
+            >
+              <svg
+                width="15"
+                height="15"
+                fill="currentColor"
+                viewBox="0 0 1792 1792"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1376 768q40 0 68 28t28 68v576q0 40-28 68t-68 28h-960q-40 0-68-28t-28-68v-576q0-40 28-68t68-28h32v-320q0-185 131.5-316.5t316.5-131.5 316.5 131.5 131.5 316.5q0 26-19 45t-45 19h-64q-26 0-45-19t-19-45q0-106-75-181t-181-75-181 75-75 181v320h736z"
+                ></path>
+              </svg>
+            </span>
+            <input
+              type="password"
+              id="sign-in-email"
+              class="
+                rounded-r-lg
+                flex-1
+                appearance-none
+                border border-gray-300
+                w-full
+                py-2
+                px-4
+                bg-white
+                text-gray-700
+                placeholder-gray-400
+                shadow-sm
+                text-base
+                focus:outline-none
+                focus:ring-2
+                focus:ring-yellow-600
+                focus:border-transparent
+              "
+              placeholder="パスワード"
+            />
+          </div>
+        </div>
+        <div class="flex w-full">
+          <button
+            type="submit"
+            class="
+              py-2
+              px-4
+              bg-yellow-500
+              hover:bg-yellow-600
+              focus:ring-yellow-400 focus:ring-offset-yellow-100
+              text-white
+              w-full
+              transition
+              ease-in
+              duration-200
+              text-center text-base
+              font-semibold
+              shadow-md
+              focus:outline-none focus:ring-2 focus:ring-offset-2
+              rounded-lg
+            "
+          >
+            新規登録
+          </button>
+        </div>
+      </form>
+    </div>
+    <div class="flex items-center justify-center mt-6">
+      <a
+        href="login"
+        class="
+          inline-flex
+          items-center
+          text-xs
+          font-thin
+          text-center text-gray-500
+          hover:text-gray-700
+          dark:text-gray-100 dark:hover:text-white
+        "
+      >
+        <span class="ml-2">登録済みの方はこちらから</span>
+      </a>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
+<script>
+import "primeicons/primeicons.css";
 
-export default defineComponent({
-  name: "About",
-  components: {},
-  setup() {
-    const state = reactive({
-      msg: "Hello TypeScript",
-    });
-    return {
-      ...toRefs(state),
-    };
-  },
-});
+export default {};
 </script>
+
+<style></style>

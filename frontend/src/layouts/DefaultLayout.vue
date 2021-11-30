d1b70b471e2b2c0dcd4e4022672b221065f061ef
Daiki Hirose <speeed131@gmail.com>
2021-11-30 23:49:15 +0900

    sidebarをauthで調節

--- frontend/src/layouts/DefaultLayout.vue
+++ frontend/src/layouts/DefaultLayout.vue
@@ -1,11 +1,11 @@
 <template>
   <Header></Header>
-  <div class="relative bg-white dark:bg-gray-800">
+  <div class="relative">
     <div class="flex justify-between">
-      <div v-if="false" class="flex-grow">
+      <div v-if="loginUser" class="">
         <Sidebar />
       </div>
-      <div class="flex-grow">
+      <div class="flex-grow bg-gray-100">
         <router-view />
       </div>
     </div>
@@ -13,19 +13,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
+import { defineComponent, reactive, toRefs, ref, computed } from "vue";
 
 import Header from "@/components/layouts/Header.vue";
 import Sidebar from "@/components/layouts/Sidebar.vue";
+import { useStore } from "vuex";
 export default defineComponent({
   name: "About",
   components: { Sidebar, Header },
   setup() {
+    const store = useStore();
     const state = reactive({
       msg: "Hello TypeScript",
     });
     return {
       ...toRefs(state),
+      loginUser: computed(() => store.getters["auth/loginUser"]),
     };
   },
 });
